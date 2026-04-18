19d568b5c39ee2e8575c5d8350a32bba5f131f92
Jancker Nicolas <jancker.yauri@gmail.com>
2026-04-17 22:23:36 -0300

    Resolução tarefa 17-04

---
+++ src/ComparadorCriterioC.java
@@ -0,0 +1,29 @@
+import java.util.Comparator;
+
+
+public class ComparadorCriterioC implements Comparator<Pedido>{
+    @Override
+    public int compare(Pedido o1, Pedido o2) {
+        int resultado = 0;
+
+        if (o1.getTotalItens() == o2.getTotalItens()) {
+            if (o1.getDataPedido().equals(o2.getDataPedido())) {
+                if (o1.getIdPedido() < o2.getIdPedido()) {
+                    resultado = (int) o1.getTotalItens();
+                } else if (o1.getIdPedido() > o2.getIdPedido()) {
+                    resultado = (int) o2.getTotalItens();
+                }
+            } else if (o1.getDataPedido().isBefore(o2.getDataPedido())) {
+                resultado = (int) o2.valorFinal();
+            } else if (o1.getDataPedido().isAfter(o2.getDataPedido())) {
+                resultado = (int) o1.valorFinal();
+            }
+        } else if (o1.getTotalItens() < o2.getTotalItens()) {
+            resultado = (int) o1.getTotalItens();
+        } else if (o1.getTotalItens() > o2.getTotalItens()) {
+            resultado = (int) o2.getTotalItens();
+        }
+
+        return resultado;
+    }
+}
